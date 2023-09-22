39546300544bb487f279c257efc6da9644e8a0a9
Nanxi Li <nanxi@student.unimelb.edu.au>
2023-09-22 18:19:52 +0800

    move client folder

--- flowershop/server/server.js
+++ flowershop/server/server.js
@@ -1,18 +1,13 @@
-// IMAGE
-// import p1 from "./assets/dicoverimg.jpg";
-// import ph1 from "./assets/hover.jpg";
-// import pd1 from "./assets/detail.jpg";
-
-// const fs = require("fs");
-// const path = require("path");
-// const img = path.join(__dirname, "./assets/dicoverimg.jpg");
-// const p1 = fs.readFileSync(img, { encoding: "utf8" });
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const PORT = process.env.PORT || 8000;
+const path = require("path");
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+const sharp = require("sharp");
+sharp.cache(false);
 
 const DUMMY_PRODUCTS = [
   {
@@ -251,6 +246,6 @@ app.get("/add_ons", (req, res) => {
   res.json({ add_ons: DUMMY_ADDONS });
 });
 
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
